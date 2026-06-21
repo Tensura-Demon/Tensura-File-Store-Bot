a29a537f915900c9e7678df50d51ea45760ee809
MohammedDev-yt <animeworldday2005@gmail.com>
2026-06-21 18:39:43 +0530

    database.py

--- database.py
+++ database.py
@@ -3,9 +3,15 @@
 # Ask Doubt @AU_Bot_Discussion 
 # Owner @Mr_Mohammed_29 
 # ------------------------- #
+
 from motor.motor_asyncio import AsyncIOMotorClient
 from config import MONGO_URI
 from datetime import datetime
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
 
 client = AsyncIOMotorClient(MONGO_URI)
 db = client.filebot
@@ -13,6 +19,11 @@ db = client.filebot
 files = db.files
 users = db.users
 admins = db["admins"]
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
 
 # FILES
 async def save_file(file_id, file_unique_id, file_type, caption, thumb=None):
@@ -28,8 +39,18 @@ async def save_file(file_id, file_unique_id, file_type, caption, thumb=None):
         upsert=True
     )
     
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
 async def get_file(file_unique_id):
     return await files.find_one({"file_unique_id": file_unique_id})
+    
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
 
 # USERS
 async def add_user(user_id):
@@ -41,16 +62,27 @@ async def add_user(user_id):
         {"$setOnInsert": {"user_id": int(user_id)}},
         upsert=True
     )
+    
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
 
 async def get_all_users():
-    users_list = []
-    async for user in users.find():
-        users_list.append(user["user_id"])
-    return users_list
+    return [u["user_id"] async for u in users.find({}, {"user_id": 1, "_id": 0})]
     
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
 
 async def total_users():
     return await users.count_documents({})
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
 
 # ADMIN SYSTEM
 
@@ -64,26 +96,37 @@ async def add_admin_db(user_id, name, username):
             "user_id": user_id,
             "name": name,
             "username": username,
-            "joined_date": datetime.now().strftime("%d-%m-%Y %H:%M")
+            "joined_date": datetime.utcnow()
         }},
         upsert=True
     )
 
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
 async def get_all_admins():
     admins_list = []
-    async for admin in admins.find():
+    async for admin in admins.find({}, {"_id": 0}):
         admins_list.append(admin)
     return admins_list
+    
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
 
 async def remove_admin_db(user_id):
     await admins.delete_one({"user_id": int(user_id)})
 
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
 async def is_admin(user_id):
-    user_id = int(user_id)
-
-    data = await admins.find_one({"user_id": user_id})
-
-    return bool(data)
+    return await admins.find_one({"user_id": int(user_id)}) is not None
 
 # ------------------------- #
 # Don't Remove Credit 
