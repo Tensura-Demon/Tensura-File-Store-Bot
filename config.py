980a9154715ee1171a31b58b6e659c0a8f1c10d7
Tensura-Demon <tensuraslime04@gmail.com>
2026-07-01 21:19:43 +0530

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-
 # ------------------------- #
 # Don't Remove Credit 
 # Owner @Mr_Mohammed_29
@@ -10,11 +9,10 @@ def must_get(name):
     value = os.getenv(name)
     if not value:
         raise Exception(f"{name} is not set in environment variables")
-    return value
+    return value.strip()
 
 # ------------------------- #
-# Don't Remove Credit 
-# Owner @Mr_Mohammed_29
+# CORE CONFIG
 # ------------------------- #
 
 API_ID = int(must_get("API_ID"))
@@ -22,33 +20,28 @@ API_HASH = must_get("API_HASH")
 BOT_TOKEN = must_get("BOT_TOKEN")
 
 # ------------------------- #
-# Don't Remove Credit 
-# Owner @Mr_Mohammed_29
+# DATABASE
 # ------------------------- #
 
 MONGO_URI = must_get("MONGO_URI")
 
 # ------------------------- #
-# Don't Remove Credit 
-# Owner @Mr_Mohammed_29
+# BOT INFO
 # ------------------------- #
 
 BOT_USERNAME = must_get("BOT_USERNAME")
 
 CHANNEL_ID = int(must_get("CHANNEL_ID"))
-
 LOG_CHANNEL = int(must_get("LOG_CHANNEL"))
 
 # ------------------------- #
-# Don't Remove Credit 
-# Owner @Mr_Mohammed_29
+# OWNER
 # ------------------------- #
 
 OWNER_ID = int(must_get("OWNER_ID"))
 
-PORT = int(os.getenv("PORT", "10000"))
+# ------------------------- #
+# SERVER
+# ------------------------- #
 
-# ------------------------- #
-# Don't Remove Credit 
-# Owner @Mr_Mohammed_29
-# ------------------------- #
+PORT = int(os.getenv("PORT", "10000"))
